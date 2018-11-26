ec397e46c38ea051a9a8ad7047f04f9f43608881
EvModder <evmodder@gmail.com>
2018-11-26 02:09:54 -0600

    head stacking patch

--- src/Evil_Code_DropHeads/BlockBreakListener.java
+++ src/Evil_Code_DropHeads/BlockBreakListener.java
@@ -1,10 +1,10 @@
 package Evil_Code_DropHeads;
 
-import net.md_5.bungee.api.ChatColor;
 import net.minecraft.server.v1_12_R1.BlockPosition;
 import net.minecraft.server.v1_12_R1.TileEntitySkull;
 
 import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
