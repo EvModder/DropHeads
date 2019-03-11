92c58186c7287703420a6c90ded4d8de1337a24e
EvModder <evmodder@gmail.com>
2019-03-11 00:01:07 -0700

    updated to standard ev-commands formats

---
+++ src/EvLib2/CommandBase2.java
@@ -0,0 +1,29 @@
+package EvLib2;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class CommandBase2 implements CommandExecutor{
+//	protected EvPlugin plugin;
+	String commandName;
+	final static CommandExecutor disabledCmdExecutor = new CommandExecutor(){
+		@Override
+		public boolean onCommand(CommandSender sender, Command command, String label, String args[]){
+			sender.sendMessage(ChatColor.RED+"This command is currently unavailable");
+			return true;
+		}
+	};
+
+	public CommandBase2(JavaPlugin pl, boolean enabled){
+//		plugin = pl;
+		commandName = getClass().getSimpleName().substring(7).toLowerCase();
+		pl.getCommand(commandName).setExecutor(enabled ? this : disabledCmdExecutor);//TODO: fix? test?
+	}
+
+	public CommandBase2(JavaPlugin pl){
+		this(pl, true);
+	}
+}
--- src/Evil_Code_DropHeads/DropHeads.java
+++ src/Evil_Code_DropHeads/DropHeads.java
@@ -1,16 +1,6 @@
 package Evil_Code_DropHeads;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
 import EvLib2.EvPlugin;
-import EvLib2.Extras;
 import EvLib2.Updater;
 
 public final class DropHeads extends EvPlugin {
@@ -26,56 +16,8 @@ public final class DropHeads extends EvPlugin {
 		getServer().getPluginManager().registerEvents(new EntityDeathListener(), this);
 		//getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);//Fixes stacking
 		getServer().getPluginManager().registerEvents(new ItemDropListener(), this);//Fixes stacking
-	}
 
-	@Override @SuppressWarnings("deprecation")
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]){
-		if((sender instanceof Player) == false){
-			sender.sendMessage(ChatColor.RED+"This command can only be run by in-game players!");
-		}
-		else if(cmd.getName().equals("spawnhead")){
-			String target, data, str = args.length == 0 ? sender.getName(): String.join(" ", args)
-					.replace('|', ':').replace('-', ':').replace(' ', '_');
-			int i = str.indexOf(':');
-			if(i != -1){
-				target = str.substring(0, i);
-				data = str.substring(i+1).toUpperCase();
-			}
-			else{
-				target = str;
-				data = null;
-			}
-			ItemStack head = null;
-			OfflinePlayer p = null;
-
-			if(target.toUpperCase().startsWith("MHF_")){
-				head = new ItemStack(Material.PLAYER_HEAD);
-				SkullMeta meta = (SkullMeta) head.getItemMeta();
-				meta.setOwner(target);
-				EntityType type = Utils.getEntityByName(target);
-				meta.setDisplayName(ChatColor.WHITE+(type != null ? Utils.getNormalizedName(type)+" Head" : target));
-				head.setItemMeta(meta);
-			}
-			else if(Utils.getEntityByName(target) != null){
-				sender.sendMessage("Getting entity head with data value: "+data);
-				head = Utils.getHead(Utils.getEntityByName(target), data);
-			}
-			else if((p = getServer().getOfflinePlayer(target)) != null
-					&& (p.hasPlayedBefore() || Extras.checkExists(p.getName()))){
-				head = Utils.getPlayerHead(p.getUniqueId(), p.getName());
-			}
-			else if(target.length() > 50){
-				head = Utils.makeTextureSkull(target);
-			}
-			if(head != null){
-				((Player)sender).getInventory().addItem(head);
-				sender.sendMessage(ChatColor.GREEN+"Spawned Head: " + ChatColor.GOLD +
-						(head.hasItemMeta() && head.getItemMeta().hasDisplayName()
-								? head.getItemMeta().getDisplayName()//.substring(2)//TODO: strip leading color?
-								: head.getType().toString()));
-			}
-			else sender.sendMessage(ChatColor.RED+"Head \""+target+"\" not found");
-		}
-		return true;
+		new CommandSpawnHead(this);
+		new Commanddebug_all_heads(this);
 	}
 }
