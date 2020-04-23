e3fe3f2917961418954b485de30067502e13b891
EvModder <evmodder@gmail.com>
2020-04-23 07:13:52 -0700

    Updated excludes and release notes for v3.3.2

--- src/net/evmodder/DropHeads/listeners/BlockBreakListener.java
+++ src/net/evmodder/DropHeads/listeners/BlockBreakListener.java
@@ -10,7 +10,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import com.mojang.authlib.GameProfile;
