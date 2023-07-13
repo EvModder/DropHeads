d9c9857b0ee1f4507b4939301e07196d5cf79410
EvModder <evmodder@gmail.com>
2023-07-13 23:43:26 +0200

    Picked sound effects for mobs with missing ambient sounds

--- src/net/evmodder/DropHeads/listeners/NoteblockPlayListener.java
+++ src/net/evmodder/DropHeads/listeners/NoteblockPlayListener.java
@@ -19,6 +19,64 @@ public class NoteblockPlayListener implements Listener{
 		dhNamespaceKey = DropHeads.getPlugin().getAPI().getDropHeadsNamespacedKey();
 	}
 
+	public Sound getAmbientSound(String eType){
+		switch(eType){
+			case "ALLAY":
+				return  Sound.valueOf("ENTITY_ALLAY_AMBIENT_WITHOUT_ITEM"); //TODO: with item?
+			case "ARMOR_STAND":
+				return Sound.ENTITY_ARMOR_STAND_PLACE;
+			case "LEASH_HITCH":
+				return Sound.ENTITY_LEASH_KNOT_PLACE;
+			case "GIANT":
+				return Sound.ENTITY_ZOMBIE_AMBIENT; //TODO: play at 2x volume?
+			case "MINECART":
+			case "MINECART_CHEST":
+			case "MINECART_COMMAND":
+			case "MINECART_FURNACE":
+			case "MINECART_HOPPER":
+			case "MINECART_MOB_SPAWNER":
+			case "MINECART_TNT":
+				return Sound.ENTITY_MINECART_RIDING;
+			case "PUFFERFISH":
+				return Sound.ENTITY_PUFFER_FISH_AMBIENT;
+			case "SNIFFER":
+				return Sound.valueOf("ENTITY_SNIFFER_IDLE");
+			case "TADPOLE":
+				return Sound.valueOf("ENTITY_TADPOLE_FLOP");
+			case "TURTLE":
+				return Sound.ENTITY_TURTLE_AMBIENT_LAND;
+			case "CAVE_SPIDER":
+				return Sound.ENTITY_SPIDER_AMBIENT;
+			case "CREEPER":
+				return Sound.ENTITY_CREEPER_PRIMED;
+			case "SLIME":
+				return Sound.ENTITY_SLIME_HURT;
+			case "SNOWMAN":
+				return Sound.ENTITY_SNOW_GOLEM_AMBIENT;
+			case "MAGMA_CUBE":
+				return Sound.ENTITY_MAGMA_CUBE_HURT;
+			case "MUSHROOM_COW":
+				return Sound.ENTITY_COW_AMBIENT;
+			case "AXOLOTL":
+				return Sound.valueOf("ENTITY_AXOLOTL_IDLE_AIR"); //TODO: ENTITY_AXOLOTL_IDLE_WATER?
+			case "TRADER_LLAMA":
+				return Sound.ENTITY_LLAMA_AMBIENT;
+			case "IRON_GOLEM":
+				return Sound.ENTITY_IRON_GOLEM_HURT;
+			case "BEE":
+				return Sound.valueOf("ENTITY_BEE_LOOP");
+			case "BEE|ANGRY":
+				return Sound.valueOf("ENTITY_BEE_LOOP_AGGRESSIVE");
+			case "BOAT":
+			case "CHEST_BOAT":
+				return Sound.ENTITY_BOAT_PADDLE_WATER;
+			case "UNKNOWN":
+				return Sound.AMBIENT_CAVE;
+			default:
+				return Sound.valueOf("ENTITY_"+eType+"_AMBIENT");
+		}
+	}
+
 	@EventHandler(ignoreCancelled = true)
 	public void onNoteblockPlay(NotePlayEvent evt){
 		final Block nb = evt.getBlock();
@@ -28,14 +86,15 @@ public class NoteblockPlayListener implements Listener{
 		if(!profile.getName().startsWith(dhNamespaceKey)) return;
 		int endIdx = profile.getName().indexOf('|');
 		if(endIdx == -1) endIdx = profile.getName().indexOf('>');
+		else if(profile.getName().startsWith("BEE|ANGRY", dhNamespaceKey.length())) endIdx = dhNamespaceKey.length() + 9;
 		final String entityName = profile.getName().substring(dhNamespaceKey.length(), endIdx != -1 ? endIdx : profile.getName().length());
-		final String soundName = "ENTITY_"+entityName+"_AMBIENT";
 		try{
-			nb.getWorld().playSound(nb.getLocation(), Sound.valueOf(soundName), SoundCategory.RECORDS, /*volume=*/1f, /*pitch=*/1f);
+			final Sound sound = getAmbientSound(entityName);
+			nb.getWorld().playSound(nb.getLocation(), sound, SoundCategory.RECORDS, /*volume=*/1f, /*pitch=*/1f);
 			evt.setCancelled(true);
 		}
 		catch(IllegalArgumentException e){
-			DropHeads.getPlugin().getLogger().warning("Unable to find sound effect: "+soundName);
+			DropHeads.getPlugin().getLogger().warning("Unable to find sound effect for entity: "+entityName);
 		}
 	}
 }
